7127dca51c516d4609e2976ffc837bbe4a474ff5
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-08-01 18:56:10 -0300

    using timestamptz on date updates, hour and minute is relevant.

--- backend/tests/testdata/init-db.sql
+++ backend/tests/testdata/init-db.sql
@@ -13,7 +13,7 @@ CREATE TABLE court_case (
 CREATE TABLE case_update (
     id               UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     cnj              TEXT NOT NULL,
-    update_date      DATE NOT NULL,
+    update_date      TIMESTAMPTZ NOT NULL,
     update_details   TEXT NOT NULL,
 	created_at       TIMESTAMPTZ DEFAULT NOW(),
     FOREIGN KEY (cnj) REFERENCES court_case(cnj) ON DELETE CASCADE
@@ -21,7 +21,8 @@ CREATE TABLE case_update (
 
 INSERT INTO court_case (cnj, plaintiff, defendant, court_of_origin, start_date) VALUES
 ('5001682-88.2024.8.13.0672', 'Alice Johnson', 'Bob Smith', 'TJSP', '2024-01-15'),
-('6772130-04.2024.8.13.0161', 'Chris Miller', 'Jessica Taylor', 'TJSP', '2024-04-05');
+('6772130-04.2024.8.13.0161', 'Chris Miller', 'Jessica Taylor', 'TJSP', '2024-04-05'),
+('3562061-02.2024.8.13.0431', 'Michael Brown', 'Sarah Davis', 'TJSP', '2024-02-20');
 
 INSERT INTO case_update (cnj, update_date, update_details) VALUES
 ('5001682-88.2024.8.13.0672', '2024-07-31T10:00:00Z', 'Initial hearing scheduled for August 15, 2024.'),
--- postgres/init-db.sql
+++ postgres/init-db.sql
@@ -13,7 +13,7 @@ CREATE TABLE court_case (
 CREATE TABLE case_update (
     id               UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     cnj              TEXT NOT NULL,
-    update_date      DATE NOT NULL,
+    update_date      TIMESTAMPTZ NOT NULL,
     update_details   TEXT NOT NULL,
 	created_at       TIMESTAMPTZ DEFAULT NOW(),
     FOREIGN KEY (cnj) REFERENCES court_case(cnj) ON DELETE CASCADE
