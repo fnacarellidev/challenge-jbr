1e71a3a5d1c9fdf08fabc748b432f276407ac0d5
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-07-31 23:16:34 -0300

    setting up some tests for sqlc queries

---
+++ backend/tests/sqlc_queries_test.go
@@ -0,0 +1,91 @@
+package tests
+
+import (
+	"context"
+	"log"
+	"testing"
+	"time"
+
+	"github.com/fnacarellidev/challenge-jbr/backend/.sqlcbuild/pgquery"
+	"github.com/fnacarellidev/challenge-jbr/backend/tests/testhelpers"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgtype"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type BackendUnitTestSuite struct {
+    suite.Suite
+	pgContainer *testhelpers.PostgresContainer
+	sqlcQueries *pgquery.Queries
+	ctx context.Context
+	conn *pgx.Conn
+}
+
+func (suite *BackendUnitTestSuite) SetupSuite() {
+	suite.ctx = context.Background()
+	pgContainer, err := testhelpers.CreatePostgresContainer(suite.ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	suite.pgContainer = pgContainer
+	suite.conn, err = pgx.Connect(suite.ctx, suite.pgContainer.ConnectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	suite.sqlcQueries = pgquery.New(suite.conn)
+}
+
+func (suite *BackendUnitTestSuite) TearDownTestSuite() {
+	if err := suite.pgContainer.Terminate(suite.ctx); err != nil {
+		log.Fatalf("error terminating postgres container: %s", err)
+	}
+	suite.conn.Close(suite.ctx)
+}
+
+func (suite *BackendUnitTestSuite) TestGetCourtCase() {
+	t := suite.T()
+
+	cnj := "5001682-88.2024.8.13.0672"
+	courtCase, err := suite.sqlcQueries.GetCourtCase(suite.ctx, cnj)
+	require.NoError(t, err, "should have no error")
+	assert.Equal(t, courtCase.Cnj, cnj)
+	assert.Equal(t, courtCase.CourtOfOrigin, "TJSP")
+	assert.Equal(t, courtCase.Plaintiff, "Alice Johnson")
+	assert.Equal(t, courtCase.Defendant, "Bob Smith")
+}
+
+func (suite *BackendUnitTestSuite) TestInsertCourtCase() {
+	t := suite.T()
+
+	cnj := "5001680-88.2024.8.13.0672"
+	plaintiff := "John Doe"
+	defendant := "Foo Bar"
+	courtOfOrigin := "TJNY"
+	startDate := pgtype.Timestamptz{
+		Time: time.Date(2024, 8, 1, 0, 0, 0, 0, time.UTC),
+		Valid: true,
+	}
+	_, err := suite.sqlcQueries.InsertCourtCase(suite.ctx, pgquery.InsertCourtCaseParams{
+		Cnj: cnj,
+		Plaintiff: plaintiff,
+		Defendant: defendant,
+		CourtOfOrigin: courtOfOrigin,
+		StartDate: startDate,
+	})
+	require.NoError(t, err, "should have no error")
+
+	courtCase, err := suite.sqlcQueries.GetCourtCase(suite.ctx, cnj)
+	require.NoError(t, err, "should have no error")
+	assert.Equal(t, courtCase.Cnj, cnj)
+	assert.Equal(t, courtCase.Plaintiff, plaintiff)
+	assert.Equal(t, courtCase.Defendant, defendant)
+	assert.Equal(t, courtCase.CourtOfOrigin, courtOfOrigin)
+	assert.Equal(t, courtCase.StartDate, startDate)
+}
+
+func TestBackendApiSuite(t *testing.T) {
+    suite.Run(t, new(BackendUnitTestSuite))
+}
---
+++ backend/tests/testdata/init-db.sql
@@ -0,0 +1,33 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE court_case (
+	id				UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+	cnj				TEXT NOT NULL UNIQUE,
+	plaintiff		TEXT NOT NULL,
+	defendant		TEXT NOT NULL,
+	court_of_origin TEXT NOT NULL,
+	start_date		DATE NOT NULL,
+	created_at		TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE case_update (
+    id               UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    cnj              TEXT NOT NULL,
+    update_date      DATE NOT NULL,
+    update_details   TEXT NOT NULL,
+	created_at       TIMESTAMPTZ DEFAULT NOW(),
+    FOREIGN KEY (cnj) REFERENCES court_case(cnj) ON DELETE CASCADE
+);
+
+INSERT INTO court_case (cnj, plaintiff, defendant, court_of_origin, start_date) VALUES
+('5001682-88.2024.8.13.0672', 'Alice Johnson', 'Bob Smith', 'TJSP', '2024-01-15'),
+('6772130-04.2024.8.13.0161', 'Chris Miller', 'Jessica Taylor', 'TJSP', '2024-04-05');
+
+INSERT INTO case_update (cnj, update_date, update_details) VALUES
+('5001682-88.2024.8.13.0672', '2024-07-31T10:00:00Z', 'Initial hearing scheduled for August 15, 2024.'),
+('5001682-88.2024.8.13.0672', '2024-08-01T14:30:00Z', 'Plaintiff submitted additional evidence.'),
+('5001682-88.2024.8.13.0672', '2024-08-02T09:00:00Z', 'Defendant requested a delay for response.'),
+('6772130-04.2024.8.13.0161', '2024-07-30T11:00:00Z', 'Case file reviewed by judge.'),
+('6772130-04.2024.8.13.0161', '2024-08-01T09:30:00Z', 'Witness statements collected.'),
+('6772130-04.2024.8.13.0161', '2024-08-02T13:45:00Z', 'Defendant’s lawyer filed a motion for dismissal.'),
+('6772130-04.2024.8.13.0161', '2024-08-03T10:00:00Z', 'Hearing date scheduled for August 10, 2024.');
