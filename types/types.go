1f3d2a5fa4a70277cc9f6e03ae7df5ee06afea4f
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-07-31 21:08:03 -0300

    fixing wrong json output

--- types/types.go
+++ types/types.go
@@ -6,8 +6,8 @@ type CourtCase struct {
 	Cnj           string    `json:"cnj"`
 	Plaintiff     string    `json:"plaintiff"`
 	Defendant     string    `json:"defendant"`
-	CourtOfOrigin string    `json:"courtOfOrigin"`
-	StartDate     time.Time `json:"startDate"`
+	CourtOfOrigin string    `json:"court_of_origin"`
+	StartDate     time.Time `json:"start_date"`
 }
 
 type CaseUpdate struct {
