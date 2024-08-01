f94cb9008cf21b222455bbab4190b4fc685efd71
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-08-01 17:43:15 -0300

    this setup is way better than using atlas

--- postgres/init-db.sql
+++ postgres/init-db.sql
@@ -1,3 +1,24 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE court_case (
+	id				UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+	cnj				TEXT NOT NULL UNIQUE,
+	plaintiff		TEXT NOT NULL,
+	defendant		TEXT NOT NULL,
+	court_of_origin TEXT NOT NULL,
+	start_date		DATE NOT NULL,
+	created_at		TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE TABLE case_update (
+    id               UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    cnj              TEXT NOT NULL,
+    update_date      DATE NOT NULL,
+    update_details   TEXT NOT NULL,
+	created_at       TIMESTAMPTZ DEFAULT NOW(),
+    FOREIGN KEY (cnj) REFERENCES court_case(cnj) ON DELETE CASCADE
+);
+
 INSERT INTO court_case (cnj, plaintiff, defendant, court_of_origin, start_date) VALUES
 ('5001682-88.2024.8.13.0672', 'Alice Johnson', 'Bob Smith', 'TJSP', '2024-01-15'),
 ('3562061-02.2024.8.13.0431', 'Michael Brown', 'Sarah Davis', 'TJSP', '2024-02-20'),
