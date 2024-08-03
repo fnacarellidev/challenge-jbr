74e84131098b09ea8ebba3b8ac756a1679e9c541
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-08-03 15:49:41 -0300

    fix: using className

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -2,12 +2,12 @@ import "./styles.css"
 
 export default function SearchPage() {
 	return (
-		<div class="search-page-wrapper">
-			<h1 class="search-page-title">Buscar</h1>
+		<div className="search-page-wrapper">
+			<h1 className="search-page-title">Buscar</h1>
 			<h3>Busque um processo a partir do número unificado</h3>
-			<div class="search-bar-wrapper">
-				<input class="search-page-input" type="text" placeholder="Número de processo" />
-				<button class="search-page-button">Buscar</button>
+			<div className="search-bar-wrapper">
+				<input className="search-page-input" type="text" placeholder="Número de processo" />
+				<button className="search-page-button" onClick={() => console.log('hello')}>Buscar</button>
 			</div>
 		</div>
 	)
