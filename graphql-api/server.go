684f1c86ade9693bb5e150a2735c9406acf2b0eb
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-08-03 16:18:46 -0300

    shutting cors down

--- graphql-api/server.go
+++ graphql-api/server.go
@@ -80,13 +80,29 @@ func AddCourtCase(p graphql.ResolveParams) (interface{}, error) {
 	}, nil
 }
 
+func disableCors(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Authorization, Content-Type, Content-Length, Accept-Encoding")
+
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Max-Age", "86400")
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
+}
+
 func main() {
     h := handler.New(&handler.Config{
         Schema: schemainit.SchemaInit(FetchBackendCourtCase, AddCourtCase),
         Pretty: true,
     })
 
-    http.Handle("/graphql", h)
+    http.Handle("/graphql", disableCors(h))
     log.Println("Server is running on port 8080")
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
