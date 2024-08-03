151e50377b3bb648aff484402d593ee5567f88ea
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-08-03 17:05:57 -0300

    redirecting on successfull fetch, not passing info yet

--- frontend/src/pages/SearchPage.js
+++ frontend/src/pages/SearchPage.js
@@ -1,12 +1,19 @@
-import React, { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react"
 import "./styles.css"
 
 export default function SearchPage() {
 
 	const [cnj, setCnj] = useState("")
 	const [err, setErr] = useState(false)
+	const [errMsg, setErrMsg] = useState("")
+	const navigate = useNavigate();
 
-	async function fetchStuff() {
+	useEffect(() => {
+		if (!err && cnj) navigate("/case")
+	}, [err])
+
+	async function fetchCourtCase() {
 		const endpoint = "http://localhost:8080/graphql"
 		const query = `{
 			"query": "query($cnj: String!) { court_case(cnj: $cnj) { cnj plaintiff defendant court_of_origin start_date updates { update_date update_details } } }",
@@ -24,6 +31,8 @@ export default function SearchPage() {
 
 		const graphql = await res.json()
 		setErr('errors' in graphql)
+		if ('errors' in graphql)
+			setErrMsg(`⚠️ Não foi encontrado nenhum processo de cnj ${cnj}`)
 	}
 
 	return (
@@ -31,11 +40,11 @@ export default function SearchPage() {
 			<h1 className="search-page-title">Buscar</h1>
 			<h3>Busque um processo a partir do número unificado</h3>
 			{ err &&
-				<p className="search-page-error-msg">⚠️ Não foi encontrado nenhum processo de cnj {cnj}</p>
+				<p className="search-page-error-msg">{errMsg}</p>
 			}
 			<div className="search-bar-wrapper">
 				<input className="search-page-input" type="text" onChange={(e) => setCnj(e.target.value)} placeholder="Número de processo" />
-				<button className="search-page-button" onClick={fetchStuff}>Buscar</button>
+				<button className="search-page-button" onClick={fetchCourtCase}>Buscar</button>
 			</div>
 		</div>
 	)
