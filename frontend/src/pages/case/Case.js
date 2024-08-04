e3c92d259c09d0ead15531d9387bd0a46ef35e2a
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-08-04 01:43:58 -0300

    styles for case page, basically done

--- frontend/src/pages/case/Case.js
+++ frontend/src/pages/case/Case.js
@@ -17,16 +17,21 @@ export default function Case() {
 
 	return (
 		<>
-			<h1>Processo n. { courtCase.court_case.cnj } do { courtCase.court_case.court_of_origin }</h1>
-			<p>Distribuído em { formatDate(courtCase.court_case.start_date) }</p>
-			<p>{ courtCase.court_case.plaintiff } vs { courtCase.court_case.defendant }</p>
-			<div>Movimentações</div>
-			{ courtCase.court_case.updates.map((update) => (
-				<>
-					<p>{ formatDate(update.update_date) }</p>
-					<p>{ update.update_details }</p>
-				</>
-			))}
+			<div style={{ margin: '16px 34px' }}>
+				<h2 style={{ fontWeight: '400' }}>
+					Processo n. { courtCase.court_case.cnj } do { courtCase.court_case.court_of_origin }
+				</h2>
+				<p style={{ margin: '4px 0' }}>
+					Distribuído em { formatDate(courtCase.court_case.start_date) } | { courtCase.court_case.plaintiff } vs { courtCase.court_case.defendant }
+				</p>
+				<div class="updates-title-box">Movimentações</div>
+				{ courtCase.court_case.updates.map((update) => (
+					<div className="update-box-wrapper">
+					<p style={{ marginBottom: '8px'}}>{ formatDate(update.update_date) }</p>
+						<p>{ update.update_details }</p>
+					</div>
+				))}
+			</div>
 		</>
 	)
 }
