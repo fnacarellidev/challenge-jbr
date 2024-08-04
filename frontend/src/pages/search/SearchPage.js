b4528c1ad6361041ea8d4f6aa954e50bd45d6115
Fabio Nacarelli <97247063+fnacarellidev@users.noreply.github.com>
2024-08-04 16:19:08 -0300

    add btn register case

--- frontend/src/pages/search/SearchPage.js
+++ frontend/src/pages/search/SearchPage.js
@@ -1,13 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import SearchBar from "../../components/SearchBar"
 import "./styles.css"
 
 export default function SearchPage() {
+	const navigate = useNavigate()
 
 	return (
 		<div className="search-page-wrapper">
 			<h1 className="search-page-title">Buscar</h1>
-			<h3 style={{ textAlign: 'center', fontWeight: '400'}}>Busque um processo a partir do número unificado</h3>
+			<h3 style={{ textAlign: 'center', fontWeight: '400' }}>Busque um processo a partir do número unificado</h3>
 			<SearchBar />
+			<div className="div-center">
+			<button className="pretty-btn" style={{ marginTop: '8px'}} onClick={() => navigate("/register_case")}>Register Case</button>
+			</div>
 		</div>
 	)
 }
